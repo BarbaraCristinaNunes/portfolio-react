bee5dae2fdc70ce1dfdca896066ed11056723e74
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2023-01-04 10:33:38 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import NavBar from './Components/NavBar/NavBar';
-import Experience from './Components/Experience/Experience';
-import About from './Components/About/About';
-import Projects from './Components/Projects/Projects';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar/>
-    <About/>
-    <Experience/>
-    <Projects/>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
 
