f28baac7f1907278698e669a8d67f02d5e3ae961
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2023-01-12 09:13:59 +0100

    react-router-dom is not working yet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Experience from './Components/Experience/Experience';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const routes = [
+    {
+      path: "/",
+      component: <About/>
+    },
+    {
+      path: "/about",
+      component: <About/>
+    },
+    {
+      path: "/experience",
+      component: <Experience/>
+    },
+    {
+      path: "/projects",
+      component: <Projects/>
+    },
+  ]
   return (
     <>
+    {/* <NavBar/> */}
+    <About/>
+    <Experience/>
+    <Projects/>
+    {/* <Router>
       <NavBar/>
-      <About/>
-      <Experience/>
-      <Projects/>
+      <Routes>
+        <Route 
+          path="/about"
+        />
+
+        <Route 
+          path="/experience"
+        >
+          <Experience/>
+        </Route>
+        <Route 
+          path="/projects"
+        />
+        </Routes>
+    </Router> */}
     </>
   );
 }
