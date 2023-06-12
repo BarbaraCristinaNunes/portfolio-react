3529550c09a0481eae822edab61f3d9ee42c936d
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2023-06-12 14:30:58 +0200

    remove ListItem and ListItemText form grid

--- src/Components/About/SkillList.js
+++ src/Components/About/SkillList.js
@@ -5,7 +5,7 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Image from '../Shared/ImageStyle';
 import Text from '../Shared/TextStyle';
-import { Grid } from '@mui/material';
+
 
 export default function AlignItemsList(props) {
   return (
@@ -14,63 +14,45 @@ export default function AlignItemsList(props) {
             props.skills.map((skill) => {
                 return(
                     <ListItem alignItems="center" key={`about${skill}`}>
-                        <Grid
-                            container
-                            direction="row"
-                        >
-                            <Grid 
-                                item
-                                lg={6}
-                                md={6}
-                                xs={12}
-                            >
-                                <ListItemAvatar sx={{marginRight: 5}}>
-                                    {
-                                        skill.toLowerCase() === "material ui" ?
-                                            <Image 
-                                                alt={skill} 
-                                                src={`img/skills/mui.png`}
-                                                variant="square"
-                                            />
-                                        :
-                                            <Image 
-                                                alt={skill} 
-                                                src={`img/skills/${skill.toLowerCase()}.png`}
-                                                variant="square"
-                                            />
-                                    }
-                                </ListItemAvatar>
-                            </Grid>
-                            <Grid 
-                                item
-                                lg={6}
-                                md={6}
-                                xs={12}
-                            >
-                                <ListItemText
-                                    primary={
-                                        skill === "Rails" ?
-                                            <React.Fragment>
-                                                <Text
-                                                    paragraph="Ruby on Rails"
-                                                    aligment="justify"
-                                                    fontWeight="bold"
-                                                    time={0}
-                                                /> 
-                                            </React.Fragment>
-                                        :
-                                            <React.Fragment>
-                                                <Text
-                                                    paragraph={skill}
-                                                    aligment="justify"
-                                                    fontWeight="bold"
-                                                    time={0}
-                                                /> 
-                                            </React.Fragment>
-                                    }
-                                /> 
-                            </Grid>
-                        </Grid>
+                        <ListItemAvatar sx={{marginRight: 5}}>
+                        {
+                            skill.toLowerCase() === "material ui" ?
+                                <Image 
+                                    alt={skill} 
+                                    src={`img/skills/mui.png`}
+                                    variant="square"
+                                />
+                            :
+                                <Image 
+                                    alt={skill} 
+                                    src={`img/skills/${skill.toLowerCase()}.png`}
+                                    variant="square"
+                                />
+                        }
+                        
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                skill === "Rails" ?
+                                    <React.Fragment>
+                                        <Text
+                                            paragraph="Ruby on Rails"
+                                            aligment="justify"
+                                            fontWeight="bold"
+                                            time={0}
+                                        /> 
+                                    </React.Fragment>
+                                :
+                                    <React.Fragment>
+                                        <Text
+                                            paragraph={skill}
+                                            aligment="justify"
+                                            fontWeight="bold"
+                                            time={0}
+                                        /> 
+                                    </React.Fragment>
+                            }
+                        />
                     </ListItem>
                 )
             })
